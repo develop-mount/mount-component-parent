2ded7c77299b394066668f34234faf442632c562
lin feng <scstlinfeng@aliyun.com>
2023-08-25 16:58:43 +0800

    添加发布maven仓库配置

--- mount-l2cache-starter/src/main/java/icu/develop/l2cache/annotation/L2Cacheable.java
+++ mount-l2cache-starter/src/main/java/icu/develop/l2cache/annotation/L2Cacheable.java
@@ -14,7 +14,7 @@ import java.lang.annotation.*;
  * @version ：B1.19.9
  * description ：redis 缓存注解
  * program ：bmp-prm
- * @date 2022年8月30日 09点46分
+ * @since  2022年8月30日 09点46分
  */
 
 @Target({ElementType.TYPE, ElementType.METHOD})
@@ -23,19 +23,29 @@ import java.lang.annotation.*;
 @Documented
 public @interface L2Cacheable {
 
+    /**
+     * 缓存名称
+     * @return 缓存名称
+     */
     @AliasFor("cacheNames")
     String[] value() default {L2CacheConstant.DEFAULT_CACHE_NAME};
 
+    /**
+     * 缓存名称
+     * @return 缓存名称
+     */
     @AliasFor("value")
     String[] cacheNames() default {L2CacheConstant.DEFAULT_CACHE_NAME};
 
     /**
      * FIX: 固定设置; REQUEST_ARGS: 根据请求参数拼值;EXPRESSION: EL表达式
+     * @return 缓存类型 {@see CacheKeyType}
      */
     CacheKeyType cacheKeyType() default CacheKeyType.FIX;
 
     /**
      * 缓存的key，cacheType=固定设值；此值生效
+     * @return 缓存key
      */
     String cacheKey() default "";
 
